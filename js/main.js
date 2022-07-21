ab4beee9fa225692fe66da94201a6963bc0856dd
cryptodenWH <dnstupakov@gmail.com>
2022-07-21 10:34:46 +0300

    Site

--- js/main.js
+++ js/main.js
@@ -2,22 +2,26 @@ const navBtn = document.querySelector('.nav-button')
 const mobileNav = document.querySelector('.mobile-nav')
 const body = document.body;
 
+// Click the button
 navBtn.addEventListener('click', function (event) {
     event.stopPropagation();
-    mobileNav.classList.toggle('mobile-nav-active')
-    navBtn.classList.toggle('nav-button-close')
-    body.classList.toggle('no-scroll')
+    toggleMobileNav();
 })
 
+// Click on a window outside the navigation
 window.addEventListener('click', function () {
-    console.log('Click on window');
     if (body.classList.contains('no-scroll')) {
-        body.classList.toggle('no-scroll')
-        navBtn.classList.toggle('nav-button-close')
-        mobileNav.classList.toggle('mobile-nav-active')
+        toggleMobileNav();
     }
 })
 
+// Stopping the click inside the open navigation
 mobileNav.addEventListener('click', function (event) {
     event.stopPropagation();
 })
+
+function toggleMobileNav() {
+    mobileNav.classList.toggle('mobile-nav-active')
+    navBtn.classList.toggle('nav-button-close')
+    body.classList.toggle('no-scroll')
+}
